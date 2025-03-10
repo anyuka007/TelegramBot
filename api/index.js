07d3874250a429a8df56137e8ed46a48a62a48cd
anyuka007 <anna.popova.de@gmail.com>
2025-03-10 15:04:34 +0100

    favicon

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+app.get("/favicon.ico", (req, res) => { res.status(204).end(); });
+
 app.post(`/bot${process.env.TELEGRAM_BOT}`, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
