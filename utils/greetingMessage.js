690f94a0173a33f31f1dcbbbf8c738ca542959f5
anyuka007 <anna.popova.de@gmail.com>
2025-03-22 20:28:54 +0100

    Update greeting messages to include alternative joke command options for improved user experience

--- utils/greetingMessage.js
+++ utils/greetingMessage.js
@@ -1,10 +1,10 @@
 export const greetingMessage = (language, firstName) => {
     switch (language) {
         case "uk":
-          return `Привіт, <b>${firstName}</b>! Я бот, який може показати тобі погоду. Напиши <i>погода</i> та <i>назву міста</i>, де ти хочеш дізнатися погоду. Наприклад, <i>погода Львів</i>. Якщо ти хочеш почути жарт, напиши <i>/joke</i>. Аби привітатись, напиши <i>привіт</i>. Якщо ти хочеш закінчити спілкування, напиши <i>бувай</i>.`;
+          return `Привіт, <b>${firstName}</b>! Я бот, який може показати тобі погоду. Напиши <i>погода</i> та <i>назву міста</i>, де ти хочеш дізнатися погоду. Наприклад, <i>погода Львів</i>. Якщо ти хочеш почути жарт, напиши слово <i>жарт</i> або обери команду <i>/joke</i> в меню. Аби привітатись, напиши <i>привіт</i>. Якщо ти хочеш закінчити спілкування, напиши <i>бувай</i>.`;
         case "de":
-          return `Hallo, <b>${firstName}</b>! Ich bin ein Bot, der dir das Wetter anzeigen kann. Schreibe <i>Wetter</i> und den <i>Namen der Stadt</i>, über die du das Wetter erfahren möchtest. Zum Beispiel, <i>Wetter Berlin</i>. Wenn du einen Witz hören möchtest, schreibe <i>/joke</i>. Wenn du ein Gespräch beginnen möchtest, schreibe <i>hallo</i>. Wenn du das Gespräch beenden möchtest, schreibe <i>tschüss</i>.`;
+          return `Hallo, <b>${firstName}</b>! Ich bin ein Bot, der dir das Wetter anzeigen kann. Schreibe <i>Wetter</i> und den <i>Namen der Stadt</i>, über die du das Wetter erfahren möchtest. Zum Beispiel <i>Wetter Berlin</i>. Wenn du einen Witz hören möchtest, schreibe <i>witz</i> oder wähle <i>/joke</i> im Menü. Wenn du ein Gespräch beginnen möchtest, schreibe <i>hallo</i>. Wenn du das Gespräch beenden möchtest, schreibe <i>tschüss</i>.`;
         default:
-          return `Hello, <b>${firstName}</b>! I'm a bot that can show you the weather. Write <i>weather</i> and the <i>name of the city</i> you want to know the weather. For example, <i>weather London</i>. If you want to hear a joke, write <i>/joke</i>. If you want to start a conversation, write <i>hi</i>. If you want to end the conversation, write <i>bye</i>.`;
+          return `Hello, <b>${firstName}</b>! I'm a bot that can show you the weather. Write <i>weather</i> and the <i>name of the city</i> you want to know the weather for. For example, <i>weather London</i>. If you want to hear a joke, write <i>joke</i> or select <i>/joke</i> in the menu. If you want to start a conversation, write <i>hi</i>. If you want to end the conversation, write <i>bye</i>.`;
       }
     }
