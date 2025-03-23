7192d313297b7323dc1b2d9e5404cae06498eac8
anyuka007 <anna.popova.de@gmail.com>
2025-03-23 13:11:03 +0100

    Refactor bot.js by removing unused code and improving organization for better maintainability

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 //npm i node-telegram-bot-api
 import TelegramBot from "node-telegram-bot-api";
 import "dotenv/config";
-import { jokes, activeListeningPhrases } from "./variables.js";
-import cron from "node-cron";
-import { getWeather } from "./utils/getWeather.js";
-import { pingServer } from "./utils/pingServer.js";
 import { startHandler } from "./handlers/startHandler.js";
 import { messageHandler } from "./handlers/messageHandler.js";
 import { jokeHandler } from "./handlers/jokeHandler.js";
@@ -55,48 +51,7 @@ export const botInstance = () => {
       weatherHandler(bot, msg, match)
     );
 
-    bot.on("channel_post", (msg) => {
-      const chatId = msg.chat.id;
-      console.log("msg: ", msg);
-      const usersMessage = msg.text;
-      const hi = "привіт";
-      const bye = "бувай";
-      if (usersMessage.toString().toLowerCase().includes("жарт")) {
-        const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
-        bot.sendMessage(chatId, randomJoke);
-      } else if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
-        const author = msg.author_signature;
-        let answer = author ? `Привіт, ${msg.author_signature}` : "Привіт";
-        bot.sendMessage(msg.chat.id, answer);
-      } else if (msg.text.toString().toLowerCase().indexOf(bye) === 0) {
-        const author = msg.author_signature;
-        let answer = author
-          ? `До зустрічі, ${msg.author_signature}`
-          : "До зустрічі";
-        bot.sendMessage(msg.chat.id, answer);
-      } else {
-        const randomAnswer =
-          activeListeningPhrases[
-            Math.floor(Math.random() * activeListeningPhrases.length)
-          ];
-        bot.sendMessage(chatId, randomAnswer);
-      }
-    });
-
-    // Repeats the message of user. Matches "/echo [whatever]"
-    bot.onText(/\/echo (.+)/, (msg, match) => {
-      // 'msg' is the received Message from Telegram
-      // 'match' is the result of executing the regexp above on the text content
-      // of the message
-      console.log("msg: ", msg);
-      const chatId = msg.chat.id;
-      const resp = match[1]; // the captured "whatever"
-
-      // send back the matched "whatever" to the chat
-      bot.sendMessage(chatId, resp);
-    });
-
-   // Scheduled messages
+    // Scheduled messages
     schedules(bot);
   }
   return bot;
